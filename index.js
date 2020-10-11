8b418c13376ac7977073b40e420c2f7a475c7864
kelly r maass <kmaass77627@gmail.com>
2020-10-11 15:49:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
 // Add your functions here
+function map(sourceArray, function) {
+  let newArray = []
+  for (const element of sourceArray) {
+      function();
+  } return newArray
+}
+// function mapToNegativize(sourceArray) {
+//   let newArray = []
+//   for (const element of sourceArray) {
+//     newArray.push(element*-1);
+//   } return newArray
+// }
+// 
+// function mapToNoChange(sourceArray) {
+//   return sourceArray
+// }
+// 
+// function mapToDouble(sourceArray) {
+//   let newArray = []
+//   for (const element of sourceArray) {
+//     newArray.push(element*2);
+//   } return newArray
+// }
+// 
+// function mapToSquare(sourceArray) {
+//   let newArray = []
+//   for (const element of sourceArray) {
+//     newArray.push(element**2);
+//   } return newArray
+// }
+// 
+// function reduceToTotal(sourceArray, startingPoint=0){
+//    let total = startingPoint
+//    for (let i = 0; i < sourceArray.length; i++) {
+//      total += sourceArray[i]
+//    } return total
+// }
+// 
+// 
+// function reduceToAllTrue(sourceArray) {
+//   for (const element of sourceArray) {
+//     if (!element) {
+//       return false
+//     }
+//   } return true
+//   }
+// 
+// <<<<<<< HEAD
+// 
+// 
+// function reduceToAnyTrue(sourceArray) {
+//   for (const element of sourceArray) {
+//         if (!!element) {
+//             return true;
+//         }
+//     } return false;
+// =======
+// }
+// 
+// function reduceToAnyTrue(sourceArray) {
+// >>>>>>> bd83e1b603040a217439174ca223016a1418c933
+// 
+// }
